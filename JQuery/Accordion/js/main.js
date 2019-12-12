02cb63ccc0d2c95c8f82c8915270d5bffdfc273d
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2019-12-12 16:51:00 +0300

    Templates

---
+++ JQuery/Accordion/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+   $(".title_block").click(function(event){
+      if($(".title_block").hasClass("active_block")){
+        $(".title_block").not($(this)).removeClass("active_block");
+        $(".info").not($(this).next()).slideUp(300);
+      }
+      $(this).toggleClass("active_block").next().slideToggle(300);
+   });
+
+});
---
+++ JQuery/Slick-slider/js/main.js
@@ -0,0 +1,14 @@
+$(function(){
+
+	  $('.block-slider').slick({
+    infinite: true,
+    speed: 300,
+    slidesToShow: 1,
+    fade: true,
+    cssEase: 'linear',
+    arrows: true,
+    autoplay: true,
+    autoplaySpeed: 7000
+  });
+
+});
