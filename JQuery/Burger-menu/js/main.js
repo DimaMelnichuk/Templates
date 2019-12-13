4e69015d85865a42b64c1e96286aa5daa2d23340
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2019-12-13 16:47:04 +0300

    new burger-menu

---
+++ JQuery/Burger-menu/js/main.js
@@ -0,0 +1,9 @@
+$(function(){
+
+   /***************** Menu-btn ********************/
+
+   $(".menu__burger").click(function(event){
+     $(".menu__burger,.header-menu").toggleClass("active");
+     });
+
+});
