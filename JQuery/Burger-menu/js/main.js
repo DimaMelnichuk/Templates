9f883d5fffe12dfc1c019e24d50256bbfd3fd53a
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-01-12 12:00:35 +0300

    add percentage and burger menu

--- JQuery/Burger-menu/js/main.js
+++ JQuery/Burger-menu/js/main.js
@@ -1,9 +1,23 @@
 $(function(){
 
    /***************** Menu-btn ********************/
-
    $(".menu__burger").click(function(event){
      $(".menu__burger,.header-menu").toggleClass("active");
      });
+
+   /***************** Header BG Scroll ******************/
+
+  $(function() {
+    $(window).scroll(function() {
+      var scroll = $(window).scrollTop();
+
+      if (scroll >= 20) {
+        $('.header').addClass('fixed');
+      } else {
+        $('.header').removeClass('fixed');
+      }
+    });
+  });
+
 
 });
