0c80a741148c677e8d6574eff9bfd9aef9a939b0
DimaMelnichuk <57269449+DimaMelnichuk@users.noreply.github.com>
2020-05-08 10:24:44 +0300

    Add new-gulp-template

---
+++ new-gulp-template/app/js/components/function.js
@@ -0,0 +1,39 @@
+$(function(){
+
+   /***************** Menu-btn ********************/
+   $(".menu__burger").click(function(event){
+		$(".menu__burger,.menu").toggleClass("active");
+		$("body").toggleClass("lock");
+	});
+ 
+});
+
+/***************** WebP ********************/
+function testWebP(callback) {
+	var webP = new Image();
+	webP.onload = webP.onerror = function () {
+		callback(webP.height == 2);
+	};
+	webP.src = "data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA";
+}
+testWebP(function (support) {
+	if (support == true) {
+		document.querySelector('body').classList.add('webp');
+	}
+});
+
+/***************** loaded ********************/
+if (document.querySelector('.wrapper')) {
+	document.querySelector('.wrapper').classList.add('loaded');
+}
+
+/***************** ibg adaptive inline bg ********************/
+function ibg(){
+	let ibg=document.querySelectorAll("._ibg");
+	for (var i = 0; i < ibg.length; i++) {
+	if(ibg[i].querySelector('img')){
+	ibg[i].style.backgroundImage = 'url('+ibg[i].querySelector('img').getAttribute('src')+')';
+	}
+	}
+	}
+ibg();
